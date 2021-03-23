07713b6c92cf7bc5f269244516c2d2849ec1e2c7
C. K <32439744+mariugul@users.noreply.github.com>
2021-03-23 01:30:53 +0100

    init

---
+++ MarkdownHtml/Resources/CHANGELOG.md
@@ -0,0 +1,26 @@
+# <center>About</center> 
+-------------------------
+
+## Version
+
+
+|Versions                | Release         |
+|------------------------|---------------- |
+| Current:               | v2.0.0.9-Beta   |
+| Latest:                | v2.0.0.10-Beta  |
+| Stable:                | v1.3.0.2        |
+
+## Changelog
+
+### Added
+- **19.03.2021:** Read markdown file functionality.
+- **19.03.2021:** Create html file functionality.
+- **19.03.2021:** Web browser to display the about-page.
+
+### Bug fix
+- **19.03.2021:** Read file bug.
+- **19.03.2021:** Write file bug.
+
+## Author
+**EIDEL:** *Eidsvoll Electronics*
+
---
+++ MarkdownHtml/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Resources;
+using System.Security.Cryptography;
+using System.Text;
+using Markdig;
+using Prism.Mvvm;
+
+namespace MarkdownHtml.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _html;
+        public string Html
+        {
+            get => _html;
+            set => SetProperty(ref _html, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            var changelogText = ReadChangelogFile($"{GetDebugFolderPath()}/Resources/CHANGELOG.md");
+            var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+
+            CreateHtmlFile("changelog.html", Markdown.ToHtml(changelogText, pipeline));
+        }
+
+        private void CreateHtmlFile(string path, string html)
+        {
+            using (var fs = new FileStream(path, FileMode.Create))
+            {
+                using (var w = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    w.WriteLine(html);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads the CHANGELOG markdown
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private string ReadChangelogFile(string path)
+        {
+            return !File.Exists(path) ? $"Failed to generate the desired page.\n\nMarkdown file wasn't found at '{path}'." : File.ReadAllText(path);
+        }
+
+        /// <summary>
+        /// Retrieves the path to the debug folder that runs the application.
+        /// </summary>
+        /// <returns>Path to folder.</returns>
+        private string GetDebugFolderPath()
+        {
+            return $@"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase)?.Substring(6)}";
+        }
+    }
+}
